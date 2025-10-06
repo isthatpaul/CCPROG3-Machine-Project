9015fa4a1a84aae5ff6f55cc22b4a032d37907ce
Paul Martin Ryan Crisologo <paul_crisologo@dlsu.edu.ph>
2025-10-06 23:13:42 +0800

    Added the rest of the classes

---
+++ src/Property.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Property
+{
+    private String name;
+    private double basePrice;
+    private final List<DateSlot> dates;
+    private final List<Reservation> reservations;
+}
