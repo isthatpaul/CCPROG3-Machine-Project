e3364bf952f53ddb596da0a2df32971875884b14
Paul Martin Ryan Crisologo <paul_crisologo@dlsu.edu.ph>
2025-10-06 23:23:27 +0800

    Added the rest of the classes and implemented some variables

--- src/DateSlot.java
+++ src/DateSlot.java
@@ -1,4 +1,8 @@
 public class DateSlot
 {
+    private final int dayNumber;
+    private double pricePerNight;
+    private boolean booked;
+    private Reservation reservation;
 
 }
--- src/Property.java
+++ src/Property.java
@@ -6,4 +6,17 @@ public class Property
     private double basePrice;
     private final List<DateSlot> dates;
     private final List<Reservation> reservations;
+    public static final double DEFAULT_BASE_PRICE = 1500.0;
+    public static final double MIN_BASE_PRICE = 100.0;
+
+    public Property(String name, int numDates)
+    {
+        this.name = name;
+        this.basePrice = DEFAULT_BASE_PRICE;
+        this.dates = new ArrayList<>();
+        for (int i = 1; i <= numDates; i++)
+        {
+
+        }
+    }
 }
