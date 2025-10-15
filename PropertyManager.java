3eac0e80b96296ef8270a16abc923051a6c6840d
nhmori <spencer_limun@dlsu.edu.ph>
2025-10-16 00:53:21 +0800

    Add files via upload
    Just added the format, but the content isn't final yet.

---
+++ PropertyManager.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PropertyManager
+{
+    private List<Property> properties;
+
+    public PropertyManager()
+    {
+        this.properties = new ArrayList<>();
+    }
+
+    public boolean addProperty(String name, int numDates)
+    {
+
+    }
+
+    public removeProperty(String name)
+    {
+
+    }
+
+    public getProperty(String name)
+    {
+        // should return property
+    }
+
+    public listProperties()
+    {
+        return properties;
+    }
+
+    public boolean isUniqueName(String name) 
+    {
+        
+    }
+}
