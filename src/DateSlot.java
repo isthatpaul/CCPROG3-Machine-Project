5b4e44a70b235bdbd3b9486f4dd88db377fce8be
Paul Martin Ryan Crisologo <paul_crisologo@dlsu.edu.ph>
2025-10-17 01:39:18 +0800

    created methods and contructors for the class

--- src/DateSlot.java
+++ src/DateSlot.java
@@ -1,8 +1,61 @@
-public class DateSlot
-{
-    private final int dayNumber;
+public class DateSlot {
+    private final int dayNumber;       // 1..n
     private double pricePerNight;
     private boolean booked;
     private Reservation reservation;
 
+    public DateSlot(int dayNumber, double basePrice)
+    {
+        this.dayNumber = dayNumber;
+        this.pricePerNight = basePrice;
+        this.booked = false;
+        this.reservation = null;
+    }
+
+    public int getDayNumber()
+    {
+        return dayNumber;
+    }
+
+    public double getPricePerNight()
+    {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight)
+    {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public boolean isBooked()
+    {
+        return booked;
+    }
+
+    public Reservation getReservation()
+    {
+        return reservation;
+    }
+
+    public boolean isAvailable()
+    {
+        return !booked;
+    }
+
+    public boolean book(Reservation res)
+    {
+        if (booked) {
+            return false;
+        } else {
+            booked = true;
+            reservation = res;
+            return true;
+        }
+    }
+
+    public void cancelBooking()
+    {
+        booked = false;
+        reservation = null;
+    }
 }
