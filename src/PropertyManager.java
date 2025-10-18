2cc74fb1a81453c50c5a57375a43609266100eca
Paul Martin Ryan Crisologo <paul_crisologo@dlsu.edu.ph>
2025-10-19 00:39:01 +0800

    added documentation and some validations

--- src/PropertyManager.java
+++ src/PropertyManager.java
@@ -1,4 +1,76 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 public class PropertyManager
 {
+    private List<Property> properties;
 
+    public PropertyManager()
+    {
+        this.properties = new ArrayList<>();
+    }
+
+    public boolean addProperty(String name, double basePrice)
+    {
+        if (!isUniqueName(name))
+        {
+            System.out.println("Invalid: A property named \"" + name + "\" already exists.");
+            return false;
+        }
+
+        Property newProperty = new Property(name, basePrice);
+        properties.add(newProperty);
+        System.out.println("Property \"" + name + "\" added successfully.");
+        return true;
+    }
+
+    public boolean removeProperty(String name)
+    {
+        for (int i = 0; i < properties.size(); i++)
+        {
+            Property p = properties.get(i);
+            if (p.getPropertyName().equalsIgnoreCase(name))
+            {
+                properties.remove(i);
+                System.out.println("Property \"" + name + "\" removed.");
+                return true;
+            }
+        }
+        System.out.println("No property found with the name \"" + name + "\".");
+        return false;
+    }
+
+    public Property getProperty(String name)
+    {
+        for (int i = 0; i < properties.size(); i++)
+        {
+            Property p = properties.get(i);
+            if (p.getPropertyName().equalsIgnoreCase(name))
+            {
+                return p;
+            }
+        }
+        return null;
+    }
+
+
+    public List<Property> listProperties()
+    {
+        return properties;
+    }
+
+    public boolean isUniqueName(String name)
+    {
+        for (int i = 0; i < properties.size(); i++)
+        {
+            Property p = properties.get(i);
+
+            if (p.getPropertyName().equalsIgnoreCase(name))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
