641972eaba46e474f723fb34a1315ae5287fc73f
Paul Martin Ryan Crisologo <paul_crisologo@dlsu.edu.ph>
2025-10-23 00:14:56 +0800

    added methods based on the specs

--- src/PropertyManager.java
+++ src/PropertyManager.java
@@ -17,7 +17,6 @@ public class PropertyManager
             System.out.println("Invalid: A property named \"" + name + "\" already exists.");
             return false;
         }
-
         Property newProperty = new Property(name, basePrice);
         properties.add(newProperty);
         System.out.println("Property \"" + name + "\" added successfully.");
@@ -31,6 +30,11 @@ public class PropertyManager
             Property p = properties.get(i);
             if (p.getPropertyName().equalsIgnoreCase(name))
             {
+                if (!p.canBeRemoved())
+                {
+                    System.out.println("Cannot remove \"" + name + "\": Active reservations exist.");
+                    return false;
+                }
                 properties.remove(i);
                 System.out.println("Property \"" + name + "\" removed.");
                 return true;
@@ -39,6 +43,36 @@ public class PropertyManager
         System.out.println("No property found with the name \"" + name + "\".");
         return false;
     }
+
+    public boolean renameProperty(String oldName, String newName)
+    {
+        if (!isUniqueName(newName))
+        {
+            System.out.println("Invalid: A property named \"" + newName + "\" already exists.");
+            return false;
+        }
+        Property p = getProperty(oldName);
+        if (p != null)
+        {
+            p.renameProperty(newName);
+            System.out.println("Property \"" + oldName + "\" renamed to \"" + newName + "\".");
+            return true;
+        }
+        System.out.println("No property found with the name \"" + oldName + "\".");
+        return false;
+    }
+
+    public boolean bookProperty(String name, String guestName, int checkIn, int checkOut)
+    {
+        Property p = getProperty(name);
+        if (p == null)
+        {
+            System.out.println("No property found with the name \"" + name + "\".");
+            return false;
+        }
+        return p.addReservation(guestName, checkIn, checkOut);
+    }
+
 
     public Property getProperty(String name)
     {
